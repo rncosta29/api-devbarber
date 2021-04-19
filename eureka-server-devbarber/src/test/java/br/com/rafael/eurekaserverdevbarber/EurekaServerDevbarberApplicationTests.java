28bb448e04f21bf3aa1c1e2e09c601a75778abd5
Rafael Costa <raf.costa29@gmail.com>
2021-04-18 21:08:57 -0300

    Eureka Server criado e configurado

---
+++ eureka-server-devbarber/src/test/java/br/com/rafael/eurekaserverdevbarber/EurekaServerDevbarberApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.eurekaserverdevbarber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDevbarberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
